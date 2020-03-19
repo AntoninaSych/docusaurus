34e40364eced8f27ca4dcd000d106eeae49ffbf5
antonina <antoninasych@gmail.com>
2020-03-19 15:04:18 +0200

    Сворачивать категории

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -101,7 +101,7 @@ const siteConfig = {
   twitterImage: 'img/undraw_tweetstorm.svg',
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+    docsSideNavCollapsible: true,
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
